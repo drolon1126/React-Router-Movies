e3e7bdf4a7820a42c3028333aec777ff9f65971b
danRolon <daniel.rolon@yahoo.com>
2019-07-22 18:10:24 -0500

    stretch complete: refactored repeated code, added savemovie functionality

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const SavedList = props => (
   <div className="saved-list">
     <h3>Saved Movies:</h3>
     {props.list.map(movie => (
-      <span className="saved-movie">{movie.title}</span>
+      <NavLink to={`/movies/${movie.id}`} className="saved-movie">{movie.title}</NavLink>
     ))}
-    <div className="home-button"><Link to='/'>Home</Link></div>
+    <div className="home-button"><NavLink to='/'>Home</NavLink></div>
   </div>
 );
 
